fa35b5cdba3b56c4d76c446f113332c9a6324a18
LGuijt <mail.lisageurts@gmail.com>
2025-02-14 14:06:51 +0100

    commit end

--- views/public/itempage.php
+++ views/public/itempage.php
@@ -4,9 +4,15 @@
 $file_json = file_get_contents('assets/JSON/dummydata.json');
 $file = json_decode($file_json, true);
 $products = $file['products'];
+$current_product = 1;
+$product = $products[$current_product];
 
 ?>
-<div class="main-container">
-
+<div class="main">
+    <div class="container first">
+        <a href="menu">Back to menu</a>
+        <img src="cdn/img/products/<?= $product['product_image'] ?>.png" alt="<?= $product['product_name'] ?>">
+        <h1><?= $product['product_name'] ?></h1>
+    </div>
 </div>
 <?php include __DIR__ . '/core/footer.php'; ?>
