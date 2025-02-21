aaa1e373fa5ca7587ac95829b3358754a6425d7a
LGuijt <mail.lisageurts@gmail.com>
2025-02-21 12:25:50 +0100

    menu-update

--- views/functions/categories.php
+++ views/functions/categories.php
@@ -2,14 +2,16 @@
 require_once __DIR__ . '/../../core/db_connect.php';
 require_once __DIR__ . '/../../core/db_credentials.php';
 $categories = [];
-$categoriesQuery = "SELECT category_id, name FROM categories";
+$categoriesQuery = "SELECT category_id, name, description, img FROM categories";
 $categoriesStmt = $con->prepare($categoriesQuery);
-$categoriesStmt->bind_result($category_id, $name);
+$categoriesStmt->bind_result($category_id, $name, $description, $img);
 $categoriesStmt->execute();
 while ($categoriesStmt->fetch()) {
     $categories[] = [
         'category_id' => $category_id,
-        'name' => $name
+        'name' => $name,
+        'description' => $description,
+        'img' => $img
     ];
 }
 
--- views/public/assets/js/menu.js
+++ views/public/assets/js/menu.js
@@ -1,15 +1,121 @@
-async function catApi(){
-    const res = await fetch('views/functions/categories.php', {
-    method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+async function catApi() {
+  const res = await fetch("views/functions/categories.php", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    const data = await res.json();
-    console.log(data);
-};
+  const data = await res.json();
+  neededData = data.data.categories;
+  console.log(neededData);
+  fillCat(neededData);
+}
+
+async function prodApi() {
+  const res = await fetch("views/functions/allProducts.php", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const data = await res.json();
+  neededData = data.data.products;
+  console.log(neededData);
+  fillProd(neededData);
+}
 
 catApi();
 
-console.log('Hello World!');
+prodApi();
+
+function fillCat(data) {
+  const category = document.getElementById("categories");
+  category.innerHTML = "<h2>Categories</h2>";
+  for (let i = 0; i < data.length; i++) {
+    img = data[i].img;
+    catName = data[i].name;
+    id = data[i].category_id;
+    description = data[i].description;
+
+    const cat = document.createElement("article");
+    cat.classList.add("category");
+    cat.setAttribute("id", "cat" + id);
+    cat.setAttribute("onclick", "changeCat(" + id + ")");
+
+    cat.appendChild(addimg(img));
+    cat.appendChild(setDetails("cat", catName, description));
+    category.appendChild(cat);
+  }
+}
+
+function fillProd(data) {
+  for (c = 1; c < 7; c++) {
+    const menu = document.getElementById("menu-list" + c);
+    menu.innerHTML = "";
+    for (let i = 0; i < data.length; i++) {
+        catId = data[i].category_id;
+        if (catId === c) {
+      img = data[i].image_id;
+      catName = data[i].name;
+      id = data[i].product_id;
+      price = data[i].price;
+
+      const prod = document.createElement("div");
+      prod.classList.add("menu-item");
+
+      prod.appendChild(addimg(img));
+      prod.appendChild(setDetails("product", catName, 0, price));
+      const link = document.createElement("a");
+        link.href = "itempage?sku" + id;
+        link.classList.add("item-link");
+        link.textContent = "Add to cart";
+        prod.appendChild(link);
+
+      menu.appendChild(prod);
+        }
+    }
+  }
+}
+
+function changeCat(id) {
+  console.log(id);
+}
+
+function addimg(img) {
+  const imgTag = document.createElement("img");
+  imgTag.src = "cdn/img/products/" + img + ".png";
+  return imgTag;
+}
+
+function setDetails(type, catName, description, price) {
+  const details = document.createElement("div");
+  details.classList.add("category-details");
+  details.appendChild(addName(catName));
+  if (type === "cat"){
+  details.appendChild(addDescription(description));
+}
+  if (type === "product") {
+    details.appendChild(addPrice(price));
+  }
+  return details;
+}
+
+function addName(catName) {
+  const name = document.createElement("h3");
+  name.textContent = catName;
+  return name;
+}
+
+function addDescription(description) {
+  const desc = document.createElement("p");
+  desc.textContent = description;
+  return desc;
+}
+
+function addPrice(price) {
+  const priceTag = document.createElement("p");
+  priceTag.textContent = "€" + price;
+  return priceTag;
+}
