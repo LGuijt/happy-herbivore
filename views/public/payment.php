614341879b306686f3811a7c1ff03e8bde9ccb06
LGuijt <mail.lisageurts@gmail.com>
2025-03-05 11:15:25 +0100

    quick commit

---
+++ views/public/payment.php
@@ -0,0 +1,21 @@
+<?php include __DIR__ . '/core/header.php'; ?>
+
+<div class="parentContainer">
+    <div class="orderContainer">
+        <div class="productContainer" id="productContainer">
+        </div>
+        <hr>
+        <div class="totalContainer">
+            <p class="big">Total</p>
+            <p class="big" id="priceContainer"></p>
+        </div>
+        <div class="checkoutContainer">
+            <div class="cancelOrderButton">
+                Cancel Order
+            </div>
+            <div class="checkoutButton">
+                CHECK OUT
+            </div>
+        </div>
+    </div>
+</div>
